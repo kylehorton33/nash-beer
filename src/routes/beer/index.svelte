9c1e5a5da4c269b23e56ea338374aab48d9f303b
Kyle Horton <kylehorton33@gmail.com>
2022-01-11 22:58:52 -0500

    feat: :sparkles: load data from json-server

--- src/routes/beer/index.svelte
+++ src/routes/beer/index.svelte
@@ -1,12 +1,31 @@
+<script context="module">
+	const API_ENDPOINT = process.env['API_ENDPOINT'];
+
+	export async function load({ fetch }) {
+		const res = await fetch(`${API_ENDPOINT}/beer`);
+		return {
+			props: {
+				loadedBeers: await res.json()
+			}
+		};
+	}
+</script>
+
+<script>
+	export let loadedBeers;
+</script>
+
 <div class="hero h-96 bg-base-200">
 	<div class="text-center hero-content">
 		<div class="max-w-md">
 			<h1 class="mb-5 text-5xl font-bold">Beer Page</h1>
-			<p class="mb-5">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus placeat, ullam, debitis
-				doloremque ex nam quisquam odit maiores exercitationem quaerat atque labore fugit harum fuga
-				aperiam. Vitae praesentium animi autem?
-			</p>
+			{#if loadedBeers}
+				{#each loadedBeers as beer}
+					<p>{beer.name}</p>
+				{/each}
+			{:else}
+				<p class="mb-5">Out of beer...</p>
+			{/if}
 		</div>
 	</div>
 </div>
