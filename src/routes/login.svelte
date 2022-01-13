b81ea469287492cbf45666d527b9cb5260bef3cf
Kyle Horton <kyle@cplventures.com>
2022-01-12 19:23:19 -0500

    feat: :passport_control: login logic and error display

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,21 +1,57 @@
+<script>
+	let username,
+		password = '';
+
+	let errors = [];
+
+	const handleLogin = () => {
+		console.log(username, password);
+
+		errors = [];
+
+		if (username != 'testuser') {
+			errors = ['There is no user with that name', ...errors];
+		}
+
+		if (username != 'testpassword') {
+			errors = ['Incorrect Password', ...errors];
+		}
+	};
+</script>
+
 <div class="flex flex-row justify-center mt-8 mb-16">
 	<div class="card shadow-2xl lg:card-side bg-base-300 w-96">
 		<div class="card-body">
-			<div class="form-control">
-				<label class="label">
-					<span class="label-text">Username</span>
-				</label>
-				<input placeholder="user@example.com" class="input input-bordered" type="text" />
-				<label class="label">
-					<span class="label-text">Password</span>
-				</label>
-				<input placeholder="password" class="input input-bordered" type="password" />
-				<div class="login-placeholder h-20" />
-			</div>
+			<form on:submit|preventDefault={handleLogin}>
+				<div class="form-control">
+					{#each errors as error}
+						<div class="alert alert-error mb-2">{error}</div>
+					{/each}
+					<label class="label">
+						<span class="label-text">Username</span>
+					</label>
+					<input
+						bind:value={username}
+						placeholder="user@example.com"
+						class="input input-bordered"
+						type="text"
+					/>
+					<label class="label">
+						<span class="label-text">Password</span>
+					</label>
+					<input
+						bind:value={password}
+						placeholder="password"
+						class="input input-bordered"
+						type="password"
+					/>
+					<div class="login-placeholder h-20" />
+				</div>
 
-			<div class="justify-end card-actions mt-7">
-				<button class="btn btn-primary"> Login </button>
-			</div>
+				<div class="justify-end card-actions mt-7">
+					<button class="btn btn-primary" on:click={handleLogin}> Login </button>
+				</div>
+			</form>
 			<div class="flex flex-row justify-center mt-6">
 				<a href="/signup" class="label-text-alt" data-bcup-haslogintext="no"
 					>Need a new account? Sign up here.</a
